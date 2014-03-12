46163da2be0f8a75a4b8f32e673158bb109c9070
Eystein Bye <eysteinbye@gmail.com>
2014-03-12 22:14:56 +0100

    Refactoring - Moving js code to own files

---
+++ js/controller.js
@@ -0,0 +1,34 @@
+var app = angular.module("myapp", ["firebase"]);
+
+function MyController($scope, angularFire, angularFireAuth) {
+
+    var ref = new Firebase("https://eysteinbye.firebaseio.com/showme");
+    angularFireAuth.initialize(ref, { scope: $scope, name: "user" });
+
+    angularFire(ref, $scope, "messages");
+
+    $scope.messages = [];
+
+    $scope.addMessage = function(e) {
+        if (e.keyCode != 13) return;
+
+        $scope.messages.push({
+            name: $scope.user.name,
+            product: $scope.product,
+            desc: $scope.desc
+        });
+
+        $scope.desc = "";
+        $scope.product = "";
+    };
+
+    $scope.loginFacebook = function () {
+        angularFireAuth.login("facebook");
+    };
+    $scope.loginTwitter = function () {
+        angularFireAuth.login("twitter");
+    };
+    $scope.logout = function () {
+        angularFireAuth.logout();
+    };
+}
